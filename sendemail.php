9fdf81c15c6b8a323c5bcee2683610e04fcf022d
Joshua Kovalchik <kovee98@gmail.com>
2016-10-11 00:23:12 -0500

    up late again

--- sendemail.php
+++ sendemail.php
@@ -1,17 +1,18 @@
-<?php
+<?php 
+if(isset($_POST['submit'])){
+    $to = "chemicaleuphoria98@gmail.com"; // this is your Email address
+    $from = $_POST['senderEmail']; // this is the sender's Email address
+    $name = $_POST['sender'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
-if($_POST["submit"]) {
-    $recipient="chemicaleuphoria98@gmail.com";
-    $subject="Form to email message";
-    $sender=$_POST["sender"];
-    $senderEmail=$_POST["senderEmail"];
-    $message=$_POST["message"];
-
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
-
-    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
-
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
-}
-
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent!" . $name . ", I will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
 ?>
